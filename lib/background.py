dbae223b22c8ca85b35e3104a52ed0321925d80f
Jack Freeth <jackwfreeth@gmail.com>
2025-04-08 01:56:42 +1200

    reconsolidated python commits, arranged for training

--- lib/background.py
+++ lib/background.py
@@ -8,7 +8,7 @@ from dataclasses import dataclass
 class RandomNoiseBackground:
     size: int = 1
 
-    def generate_background(self, res_x: int, res_y: int) -> Image.Image:
+    def generate_background(self, res_x: int, res_y: int, reference_images: List[str]) -> Image.Image:
         noise_array = np.random.randint(0, 256, (res_y, res_x, 3), dtype=np.uint8)
         return Image.fromarray(noise_array, "RGB")
 
@@ -19,18 +19,20 @@ class PerlinNoiseBackground:
     colour_1: Tuple[int, int, int] = (0, 0, 0)
     colour_2: Tuple[int, int, int] = (255, 255, 255)
 
-    def generate_background(self, res_x: int, res_y: int) -> Image.Image:
+    def generate_background(self, res_x: int, res_y: int, reference_images: List[str]) -> Image.Image:
         #TODO
         noise_array = np.random.randint(0, 256, (res_y, res_x, 3), dtype=np.uint8)
         return Image.fromarray(noise_array, "RGBA")
     
 @dataclass
 class RandomNoiseReferenceImageBackground:
-    reference_paths: List[str]
     noise: float = 0.0
 
-    def generate_background(self, res_x: int, res_y: int) -> Image.Image:
-        reference_image = Image.open(random.choice(self.reference_paths)).convert("RGB")
+    def generate_background(self, res_x: int, res_y: int, reference_images: List[str] ) -> Image.Image:
+
+        reference_image_path = random.choice(reference_images)
+        reference_image = Image.open(reference_image_path).convert("RGBA")
+        
         image_width, image_height = reference_image.size
         noise_array = np.random.randint(0, 256, (image_height, image_width, 3), dtype=np.uint8)
         noise_image = Image.fromarray(noise_array, "RGB")
--- lib/generator.py
+++ lib/generator.py
@@ -1,64 +1,140 @@
 from lib.background import RandomNoiseBackground, PerlinNoiseBackground, RandomNoiseReferenceImageBackground
+from lib.annotate import paste_sprite
 
+import os
 from dataclasses import dataclass, field
 from pathlib import Path
 import shutil
 from typing import List, Union
 
-from PIL import Image
+from PIL import Image, ImageEnhance, ImageFilter
 import numpy as np
 import random
 
 @dataclass
 class MutationParameters:
-    #shear in the x,y directions
+    #TODO shear in the x,y directions
     shear_x: float = 1
     shear_y: float = 1
 
     #min and max scaling of the original sprite (uniform scaling)
-    scale_min: float = 1
-    scale_max: float = 5
+    scale_min: float = 0.4
+    scale_max: float = 4
 
-    rotation: bool = False
+    brightness_min: float = 0.7
+    brightness_max: float = 1.3
+
+    contrast_min: float = 0.7
+    contrast_max: float = 1.5
+
+    sharpness_min: float = 1.5
+    sharpness_max: float = 3.0
+
+    blur_min: float = 0.5
+    blur_max: float = 2.5
+
+    rotation_min: float = -45
+    rotation_max: float = 45
+
+    effect_count = 1
 
     background: Union[RandomNoiseBackground, PerlinNoiseBackground, RandomNoiseReferenceImageBackground] = field(default_factory=RandomNoiseBackground)
 
-    def generate_background(self, res_x: int, res_y: int) -> Image.Image:
-        return self.background.generate_background(res_x, res_y)
+    enabled_ops: List[str] = field(default_factory=lambda: ["rotate", "scale", "flip", "brightness", "contrast", "sharpen", "blur"])
+
+    def generate_background(self, res_x: int, res_y: int, reference_images: List[str]) -> Image.Image:
+        return self.background.generate_background(res_x, res_y, reference_images)
+    
+    def apply_rotate(self, img):
+        angle = random.randint(self.rotation_min, self.rotation_max)
+        return img.rotate(angle, expand=True)
+
+    def apply_scale(self, img):
+        scale = random.uniform(self.scale_min, self.scale_max)
+        w, h = img.size
+        
+        #TODO investigate what is Image.LANCZOS
+        sprite_image = img.resize((int(w * scale), int(h * scale)), Image.LANCZOS)
+        #sprite_image = img.resize((int(w * scale), int(h * scale)))
+        return sprite_image
+    
+    def apply_flip(self, img):
+        return img.transpose(random.choice([Image.FLIP_LEFT_RIGHT, Image.FLIP_TOP_BOTTOM]))
+
+    def apply_brightness(self, img):
+        factor = random.uniform(self.brightness_min, self.brightness_max)
+        return ImageEnhance.Brightness(img).enhance(factor)
+
+    def apply_contrast(self, img):
+        factor = random.uniform(self.contrast_min, self.contrast_max)
+        return ImageEnhance.Contrast(img).enhance(factor)
+
+    def apply_sharpen(self, img):
+        factor = random.uniform(self.sharpness_min, self.sharpness_max)
+        return ImageEnhance.Sharpness(img).enhance(factor)
+
+    def apply_blur(self, img):
+        radius = random.uniform(self.blur_min, self.blur_max)
+        return img.filter(ImageFilter.GaussianBlur(radius))
+    
+    @property
+    def augmentation_ops(self):
+        return {
+            "rotate": self.apply_rotate,
+            "scale": self.apply_scale,
+            "flip": self.apply_flip,
+            "brightness": self.apply_brightness,
+            "contrast": self.apply_contrast,
+            "sharpen": self.apply_sharpen,
+            "blur": self.apply_blur
+        }
+
+    def apply_mutations(self, img, count: int):
+        enabled_ops = self.enabled_ops.copy()
+
+        for _ in range(count):
+            if len(enabled_ops) == 0:
+                print("Warning: Operations exhausted")
+                return img
+            
+            op_name = random.choice(enabled_ops)
+            enabled_ops.remove(op_name)
+            op = self.augmentation_ops.get(op_name)
+            if op is None:
+                raise ValueError(f"Unrecognised operation enabled: \"{op_name}\"")
+            img = op(img)
+        
+        return img
 
 
 def generate_dataset(
-        asset_paths: List[Path],
+        sprites_path: Path,
+        backgrounds_path: Path,
         save_path: Path,
         count: int,
         params: MutationParameters,
-        res_x: int = 1000,
-        res_y: int = 1000,
+        default_res_x: int = 1000,
+        default_res_y: int = 1000,
 ):
+    
+    sprite_files = [os.path.join(sprites_path, f) for f in os.listdir(sprites_path) if f.endswith(('png', 'jpg', 'jpeg'))]
+    background_files = [os.path.join(backgrounds_path, f) for f in os.listdir(backgrounds_path) if f.endswith(('png', 'jpg', 'jpeg'))]
 
     for i in range(0, count):
-        reference_image = random.choice(asset_paths)
-        with Image.open(reference_image).convert("RGBA") as img:
-            reference_img_width, reference_img_height = img.size
-            background = params.generate_background(res_x, res_y)
-            background_width, background_height = background.size
+        img_class = random.randrange(len(sprite_files))
+        sprite_image = Image.open(sprite_files[img_class]).convert("RGBA")
+        background_image = params.generate_background(default_res_x, default_res_y, background_files)
 
-            #do not scale the image so large that it is larger than the background
-            params.scale_max = min(params.scale_max, background_width / reference_img_width, background_height / reference_img_height)
-            params.scale_min = min(params.scale_min, params.scale_max)
-            
-            #randomly scale the image (both axis uniformly)
-            scale = random.uniform(params.scale_min, params.scale_max)
+        sprite_image = params.apply_mutations(sprite_image, 1)
 
-            #place the image at a random position on the background
-            random_offset_x = random.randint(0, background_width - int(scale*reference_img_width+1))
-            random_offset_y = random.randint(0, background_height - int(scale*reference_img_height+1))
+        generated_image, generated_annotation = paste_sprite(background=background_image, sprite=sprite_image, class_id=img_class)
+        
+        img_name = f"gen_img_{i:04d}.png"
+        generated_image.save(os.path.join(save_path, img_name))
 
-            img = img.resize((int(reference_img_width * scale), int(reference_img_height * scale)), Image.LANCZOS)
-
-            background.paste(img, (random_offset_x, random_offset_y), img)
-
-            background.save(save_path / f"{i}.png", format="PNG")
+        annotation_name = f"gen_img_{i:04d}.txt"
+        with open(os.path.join(save_path, annotation_name), "w") as f:
+            f.write(generated_annotation)
 
 
 
